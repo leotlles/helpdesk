3d2056ed03c8277510088ab7da365a657a3fe1e6
leotlles <tryagainleo@proton.me>
2023-03-02 21:09:09 -0300

    adicionando-consulta-chamados

--- registra_chamado.php
+++ registra_chamado.php
@@ -1,26 +1,21 @@
 <?php
 
-    echo '<pre>';
-    print_r($_POST);
-    echo '</pre>';
-
     $titulo = str_replace('#', '-', $_POST['título']);
     $categoria = str_replace('#', '-', $_POST['categoria']);
     $descricao = str_replace('#', '-', $_POST['descrição']);
 
     $texto = $titulo . '#' . $categoria . '#' . $descricao . PHP_EOL;
 
-    //implode('#', $_POST); // fazer esse exercício dps
-
-    //abrindo o arquivo
+    // Abrindo o arquivo
     $arquivo = fopen('arquivo.hd', 'a');
 
-    //escrevendo o texto
+    // escrevendo no arquivo
     fwrite($arquivo, $texto);
 
-    //fechando o arquivo
+    // fechando o arquivo
     fclose($arquivo);
 
-    header('Location: abrir_chamado.php')
+    // redirecionando para outra página
+    header('Location: abrir_chamado.php');
 
 ?>
